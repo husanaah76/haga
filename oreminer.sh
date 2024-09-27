7c0ffcbe205987dd151ede18c5a8775b4b409b80
husanaah76 <husanaah76@gmail.com>
2024-09-27 16:21:51 +0800

    Update oreminer.sh

--- oreminer.sh
+++ oreminer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #add your solana rpc endpoint
-DEFAULT_RPC_URL="......"
+DEFAULT_RPC_URL="https://mainnet.helius-rpc.com/?api-key=c2c2d31f-38d2-46f8-9f05-096cfc25f8ab"
 #add your solana pubkey path
-DEFAULT_KEY="....."
+DEFAULT_KEY="[75,79,149,190,136,242,157,156,107,239,41,3,59,252,73,178,132,164,173,53,146,236,96,72,29,229,31,141,137,49,202,101,115,34,94,195,134,50,38,97,204,164,6,25,9,111,190,226,220,206,161,57,223,208,86,140,209,153,241,50,148,98,161,223]"
 #define solana gas fee
-DEFAULT_FEE=0
+DEFAULT_FEE=1000
 #define how many cpu threads used
-DEFAULT_THREADS=10
+DEFAULT_THREADS=90
 
 RPC_URL=${1:-$DEFAULT_RPC_URL}
 KEY=${2:-$DEFAULT_KEY}
